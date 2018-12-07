716d0051957951c04149d49ac7f2aaf82a9a2296
Chung Nguyen <nguyenchung26011992@gmail.com>
2018-12-07 17:30:35 +0700

    activity log done

--- src/app/Listeners/ActivityLogListener.php
+++ src/app/Listeners/ActivityLogListener.php
@@ -3,8 +3,18 @@
 namespace Vicoders\ActivityLog\Listeners;
 
 use App\Entities\User;
+use App\Events\CoordinationSucceededEvent;
+use App\Events\DeleteCoordinatedOrderEvent;
+use App\Events\VehicleMaintenanceReminderEvent;
 use VCComponent\Laravel\User\Events\UserCreatedByAdminEvent;
+use VCComponent\Laravel\User\Events\UserDeletedEvent;
+use VCComponent\Laravel\User\Events\UserEmailVerifiedEvent;
+use VCComponent\Laravel\User\Events\UserLoggedInEvent;
+use VCComponent\Laravel\User\Events\UserRegisteredBySocialAccountEvent;
 use VCComponent\Laravel\User\Events\UserRegisteredEvent;
+use VCComponent\Laravel\User\Events\UserUpdatedByAdminEvent;
+use VCComponent\Laravel\User\Events\UserUpdatedEvent;
+use Vicoders\ActivityLog\Contracts\ActivityLogable;
 use Vicoders\ActivityLog\Entities\ActivityLog;
 
 class ActivityLogListener
@@ -46,12 +56,47 @@ class ActivityLogListener
     {
         $events->listen(
             UserCreatedByAdminEvent::class,
-            'App\Listeners\ActivityLogListener@handle'
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
         );
-
         $events->listen(
             UserRegisteredEvent::class,
-            'App\Listeners\ActivityLogListener@handle'
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            UserEmailVerifiedEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            UserLoggedInEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            UserDeletedEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            UserUpdatedByAdminEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            UserUpdatedEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            UserRegisteredBySocialAccountEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            CoordinationSucceededEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            VehicleMaintenanceReminderEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
+        );
+        $events->listen(
+            DeleteCoordinatedOrderEvent::class,
+            'Vicoders\ActivityLog\Listeners\ActivityLogListener@handle'
         );
     }
 }
